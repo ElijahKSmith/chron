b00422d8d80de04143bc2e12b6ada562dbd70fd4
ElijahKSmith <elijah@generationesports.com>
2024-06-19 18:16:56 -0500

    update initial creation migration

--- src-tauri/migrations/2024-06-19-220903_initial/up.sql
+++ src-tauri/migrations/2024-06-19-220903_initial/up.sql
@@ -2,20 +2,20 @@
 CREATE TABLE
   games (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    dailyResetTime INTEGER NOT NULL,
-    weeklyResetDay TEXT,
-    deletedAt INTEGER
+    name VARCHAR(255) NOT NULL,
+    dailyResetTime VARCHAR(5) NOT NULL,
+    weeklyResetDay VARCHAR(9),
+    deletedAt TIMESTAMP
   );
 
 CREATE TABLE
   tasks (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     gameId INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    type TEXT NOT NULL,
-    completedTime INTEGER,
-    nextReset INTEGER,
-    deletedAt INTEGER,
+    name VARCHAR(255) NOT NULL,
+    resetType VARCHAR(6) NOT NULL,
+    completedTime TIMESTAMP,
+    nextReset TIMESTAMP,
+    deletedAt TIMESTAMP,
     CONSTRAINT tasks_games_FK FOREIGN KEY (gameId) REFERENCES games (id)
   );
