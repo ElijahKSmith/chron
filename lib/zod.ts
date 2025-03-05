872885bc7df27461ede18be8a4857baeb54eacde
Elijah <me@eksmith.dev>
2025-03-05 17:25:05 -0600

    refactor: game and task creation with react-hook-form and zod
    Added zod
    
    Added shadcn Form and dependencies (react-hook-form)
    
    Refactored Game and Task creation to use shadcn and react-hook-form, using zod for validation
    
    Refs: #19

---
+++ lib/zod.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const gameSchema = z.object({
+  title: z
+    .string({ required_error: "Title is required" })
+    .trim()
+    .min(1, "Title is required"),
+  dailyHour: z
+    .number({ required_error: "Daily hour is required" })
+    .min(0)
+    .max(23),
+  dailyMinute: z
+    .number({ required_error: "Daily minute is required" })
+    .min(0)
+    .max(59),
+  weeklyDay: z
+    .number({ required_error: "Weekly day is required" })
+    .min(0)
+    .max(6),
+});
+
+export const TaskType = z.enum(["daily", "weekly"]);
+
+export const taskSchema = z.object({
+  title: z
+    .string({ required_error: "Title is required" })
+    .trim()
+    .min(1, "Title is required"),
+  type: TaskType,
+  description: z.string().trim(),
+});
