79fb9b9f06092ecb3a7c0fa55bd6087d14c4ab3c
Elijah <me@eksmith.dev>
2026-01-09 15:52:08 -0600

    chore(deps): Upgrade Packages (#35)
    * Upgraded to Node.js 24
    * Upgraded to Next.js 16 and ran migrations
    * Upgraded to Tailwind v4 and ran migrations
    * Upgraded to zod v4 and applied migrations
    * Bumped other Node and Rust dependencies
    * Added required engines to package.json

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   },
   assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
   devIndicators: {
-    appIsrStatus: false,
+    position: "bottom-left",
   },
 };
 
